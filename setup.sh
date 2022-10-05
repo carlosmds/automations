8b1e35b462bf2774d61712e157fa53e4b692aa96
Carlos Medeiros de Souza <carlosmedeirosdesouza@gmail.com>
2022-10-04 21:04:41 -0300

    update repo

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install curl 
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py \
+    && python3 get-pip.py --user && rm -rf get-pip.py
+
+python3 -m pip -V && python3 -m pip install --user ansible
+
+sudo apt-add-repository ppa:ansible/ansible \
+    && sudo apt update && sudo apt install ansible -y
+
+ansible --version && python3 -m pip show ansible
