e08060c5bef6d7a97bfdae21b869ac039105055e
Carlos Medeiros de Souza <carlosmedeirosdesouza@gmail.com>
2022-10-05 17:16:47 -0300

    update setup playbook & .sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,13 @@
 sudo apt update && sudo apt install curl python3 python3-distutils python3-apt
 
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py \
-    && python3 get-pip.py --user && rm -rf get-pip.py
+    && sudo python3 get-pip.py && rm -rf get-pip.py
 
-python3 -m pip -V && python3 -m pip install --user ansible
+sudo python3 -m pip -V && sudo python3 -m pip install ansible boto3 botocore
 
 sudo apt-add-repository ppa:ansible/ansible \
     && sudo apt update && sudo apt install ansible -y
 
-ansible --version && python3 -m pip show ansible
+sudo ansible --version && sudo python3 -m pip show ansible
+
+sudo ansible-galaxy collection install amazon.aws community.aws
